362ed076c66a705843676c5bf6463c6cabf20e04
Adam Baker <abaker@gavant.com>
2020-08-03 13:30:56 -0400

    More work

--- mixins/route-pagination.d.ts
+++
@@ -1,4 +0,0 @@
-import Route from '@ember/routing/route';
-export declare type ConcreteSubclass<T> = new (...args: any[]) => T;
-export declare type PaginationRoute = ConcreteSubclass<Route>;
-export declare function RoutePagination<T extends PaginationRoute>(RouteSubclass: T): ConcreteSubclass<Route>;
